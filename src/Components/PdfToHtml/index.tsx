2970896444747b6f27cb0551651e8450cc8f12f7
henrique casagrande <riquecasa@gmail.com>
2023-05-24 18:45:41 -0300

    fix cors 4

--- src/Components/PdfToHtml/index.tsx
+++ src/Components/PdfToHtml/index.tsx
@@ -1,4 +1,3 @@
-import { PDFDocumentProxy } from "pdfjs-dist";
 import { useEffect, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
@@ -8,12 +7,35 @@ interface PdfConverterProps {
 
 export const PdfToHtml = ({ activityLink }: PdfConverterProps) => {
   const [numPages, setNumPages] = useState<number | null>(null);
+  const [pdfContent, setPdfContent] = useState<string | null>(null);
 
   useEffect(() => {
     pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.6.172/pdf.worker.js`;
   }, []);
 
-  const onDocumentLoadSuccess = ({ numPages }: PDFDocumentProxy) => {
+  useEffect(() => {
+    const loadPDF = async () => {
+      try {
+        const response = await fetch(`${activityLink}`, { mode: "no-cors" });
+        const arrayBuffer = await response.arrayBuffer();
+        const base64Data = arrayBufferToBase64(arrayBuffer);
+        setPdfContent(base64Data);
+      } catch (error) {
+        console.error("Erro ao carregar o PDF:", error);
+      }
+    };
+
+    loadPDF();
+  }, []);
+
+  const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
+    const binary = Array.from(new Uint8Array(buffer))
+      .map((byte) => String.fromCharCode(byte))
+      .join("");
+    return btoa(binary);
+  };
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
   };
 
@@ -33,11 +55,35 @@ export const PdfToHtml = ({ activityLink }: PdfConverterProps) => {
     return <div>{pages}</div>;
   };
 
+  useEffect(() => {
+    if (pdfContent) {
+      loadPDFJS();
+    }
+  }, [pdfContent]);
+
+  const loadPDFJS = async () => {
+    try {
+      const PDFJS = await import("pdfjs-dist/legacy/build/pdf");
+      const pdf = await PDFJS.getDocument({ data: atob(pdfContent!) }).promise;
+      const numPages = pdf.numPages;
+      onDocumentLoadSuccess({ numPages });
+    } catch (error) {
+      console.error("Erro ao carregar o PDF usando pdfjs-dist:", error);
+    }
+  };
+
   return (
     <div>
-      <Document file={activityLink} onLoadSuccess={onDocumentLoadSuccess}>
-        {numPages && convertToHTML()}
-      </Document>
+      {pdfContent ? (
+        <Document
+          file={`data:application/pdf;base64,${pdfContent}`}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {numPages && convertToHTML()}
+        </Document>
+      ) : (
+        <div>Carregando PDF...</div>
+      )}
     </div>
   );
 };
